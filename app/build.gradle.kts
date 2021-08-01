606475c58dbefc739ec6cb493b974636f8941518
Sateju <juan40ts@gmail.com>
2021-08-01 23:37:23 +0200

    Update lib versions. Update gradle plugin compatible with artic fox

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
 
     defaultConfig {
         applicationId = "jjtelechea.example.basesatejuapp"
-        minSdkVersion(21)
-        targetSdkVersion(30)
+        minSdk = 21
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
 
@@ -45,11 +45,11 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.recyclerview:recyclerview:1.1.0")
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
@@ -81,6 +81,6 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.1")
-    androidTestImplementation("androidx.test:runner:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
-const val kotlinVersion = "1.4.21"
-const val hiltVersion = "2.32-alpha"
-const val navVersion = "2.3.3"
-const val lifecycleVersion = "2.2.0"
-const val roomVersion = "2.2.6"
+const val kotlinVersion = "1.5.21"
+const val hiltVersion = "2.28-alpha"
+const val navVersion = "2.3.5"
+const val lifecycleVersion = "2.3.1"
+const val roomVersion = "2.3.0"
 const val retrofitVersion = "2.9.0"
 const val okhttpVersion = "4.9.0"
 
